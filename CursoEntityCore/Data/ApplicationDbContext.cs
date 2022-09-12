0c73234398fa8ced860988e6d627fbfa47a992ab
koumghar <koumghar@iti.es>
2022-09-12 16:32:22 +0200

    - Add code first project

---
+++ CursoEntityCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CursoEntityCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoEntityCore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opciones) : base(opciones)
+        {
+        }
+
+        //Escribir modelos
+        public DbSet<Categoria> Categoria { get; set; }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ CursoEntityCore/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace CursoEntityCore.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
