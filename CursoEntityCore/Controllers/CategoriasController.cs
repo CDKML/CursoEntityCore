1511afb52cbde0a6df16a4e52454bf61ce93c785
koumghar <koumghar@iti.es>
2022-09-13 12:31:04 +0200

    - Add view for Categoria

---
+++ CursoEntityCore/Controllers/CategoriasController.cs
@@ -0,0 +1,22 @@
+﻿using CursoEntityCore.Data;
+using CursoEntityCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoEntityCore.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public readonly ApplicationDbContext _contexto;
+        
+        public CategoriasController(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            List<Categoria> listaCategorias = _contexto.Categoria.ToList();
+            return View(listaCategorias);
+        }
+    }
+}
