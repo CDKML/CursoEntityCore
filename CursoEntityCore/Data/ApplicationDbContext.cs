5ae06c63cbcdc61f7dbffa7e873be38a0159ed2a
koumghar <koumghar@iti.es>
2022-09-14 18:16:14 +0200

    - Delete Data Annotations - Add Fluent API configuration

--- CursoEntityCore/Data/ApplicationDbContext.cs
+++ CursoEntityCore/Data/ApplicationDbContext.cs
@@ -24,12 +24,38 @@ namespace CursoEntityCore.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<ArticuloEtiqueta>().HasKey(ae => new { ae.Etiqueta_Id, ae.Articulo_Id });
+            //modelBuilder.Entity<ArticuloEtiqueta>().HasKey(ae => new { ae.Etiqueta_Id, ae.Articulo_Id });
 
             //Siembra de datos en este método
-            var categoria7 = new Categoria() { Categoria_Id = 41, Nombre = "Categoría 7", FechaCreacion = new DateTime(2022, 09, 13), Activo = true};
-            var categoria8 = new Categoria() { Categoria_Id = 42, Nombre = "Categoría 8", FechaCreacion = new DateTime(2022, 09, 14), Activo = false};
-            modelBuilder.Entity<Categoria>().HasData(new Categoria[] { categoria8});
+            //var categoria7 = new Categoria() { Categoria_Id = 41, Nombre = "Categoría 7", FechaCreacion = new DateTime(2022, 09, 13), Activo = true};
+            //var categoria8 = new Categoria() { Categoria_Id = 42, Nombre = "Categoría 8", FechaCreacion = new DateTime(2022, 09, 14), Activo = false};
+            //modelBuilder.Entity<Categoria>().HasData(new Categoria[] { categoria8});
+
+            //Fluent API para Categoría
+            modelBuilder.Entity<Categoria>().HasKey(c => c.Categoria_Id);
+            modelBuilder.Entity<Categoria>().Property(c => c.Nombre).IsRequired();
+            modelBuilder.Entity<Categoria>().Property(c => c.FechaCreacion).HasColumnType("date");
+
+            //Fluent API para Artículo
+            modelBuilder.Entity<Articulo>().HasKey(a => a.Articulo_Id);
+            modelBuilder.Entity<Articulo>().Property(a => a.TituloArticulo).IsRequired().HasMaxLength(20);
+            modelBuilder.Entity<Articulo>().Property(a => a.Descripcion).IsRequired().HasMaxLength(500);
+            modelBuilder.Entity<Articulo>().Property(a => a.Fecha).HasColumnType("date");
+            //Fluent API nombre de tabla y nombre columna
+            modelBuilder.Entity<Articulo>().ToTable("Tbl_Articulo");
+            modelBuilder.Entity<Articulo>().Property(a => a.TituloArticulo).HasColumnName("Titulo");
+
+            //Fluent API para Usuario
+            modelBuilder.Entity<Usuario>().HasKey(u => u.Id);
+            modelBuilder.Entity<Usuario>().Ignore(u => u.Edad);
+
+            //Fluent API para DetalleUsuario
+            modelBuilder.Entity<DetalleUsuario>().HasKey(du => du.DetalleUsuario_Id);
+            modelBuilder.Entity<DetalleUsuario>().Property(du => du.Cedula).IsRequired();
+
+            //Fluent API para Etiqueta
+            modelBuilder.Entity<Etiqueta>().HasKey(e => e.Etiqueta_Id);
+            modelBuilder.Entity<Etiqueta>().Property(e => e.Fecha).HasColumnType("date");
 
             base.OnModelCreating(modelBuilder);
         }
