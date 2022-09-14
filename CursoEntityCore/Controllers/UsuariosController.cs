268a6b817fbf6d3eeb5c82c8ecdeed15880b292f
koumghar <koumghar@iti.es>
2022-09-14 13:26:01 +0200

    - Add AgregarDetalle form

--- CursoEntityCore/Controllers/UsuariosController.cs
+++ CursoEntityCore/Controllers/UsuariosController.cs
@@ -1,6 +1,7 @@
 ﻿using CursoEntityCore.Data;
 using CursoEntityCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEntityCore.Controllers
 {
@@ -50,7 +51,7 @@ namespace CursoEntityCore.Controllers
                 return View();
             }
 
-            var usuario = _contexto.Usuario.FirstOrDefault(c => c.Id == id);
+            var usuario = _contexto.Usuario.FirstOrDefault(u => u.Id == id);
             return View(usuario);
         }
 
@@ -70,9 +71,47 @@ namespace CursoEntityCore.Controllers
         [HttpGet]
         public IActionResult Borrar(Guid? id)
         {
-            var usuario = _contexto.Usuario.FirstOrDefault(c => c.Id == id);
+            var usuario = _contexto.Usuario.FirstOrDefault(u => u.Id == id);
             _contexto.Usuario.Remove(usuario);
             _contexto.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(Guid? id)
+        {
+            if(id == null)
+            {
+                return View();
+            }
+
+            var usuario = _contexto.Usuario.Include(d => d.DetalleUsuario).FirstOrDefault(u => u.Id == id);
+            if(usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AgregarDetalle(Usuario usuario)
+        {
+            if (usuario.DetalleUsuario.DetalleUsuario_Id == 0)
+            {
+                //Creamos los detalles para ese usuario
+                _contexto.DetalleUsuario.Add(usuario.DetalleUsuario);
+                _contexto.SaveChanges();
+
+                //Después de crear el detalle del usuario, obtenemos el usuario de la base de datos
+                //y actualizamos el campo "DetalleUsuario_Id"
+                var usuarioBd = _contexto.Usuario.FirstOrDefault(u => u.Id == usuario.Id);
+                usuarioBd.DetalleUsuario_Id = usuario.DetalleUsuario.DetalleUsuario_Id;
+                _contexto.SaveChanges();
+            }
+
             return RedirectToAction(nameof(Index));
         }
     }
